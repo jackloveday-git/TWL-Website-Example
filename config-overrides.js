a468eec841448d3f29a3daa3c54a12f20a04dc99
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2022-05-15 20:16:18 -0400

    App start adjustments

--- config-overrides.js
+++ config-overrides.js
@@ -8,3 +8,15 @@ module.exports = function override(config, env) {
 
   return config;
 };
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// app.listen(9000);
